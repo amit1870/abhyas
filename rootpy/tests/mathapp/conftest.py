bee038458f4bee3c517abcb3552a2efe5832c9b4
amit1870 <amitxvf@gmail.com>
2024-02-26 12:39:56 +0000

    day 15

--- rootpy/tests/mathapp/conftest.py
+++ rootpy/tests/mathapp/conftest.py
@@ -2,15 +2,18 @@ import pytest
 
 from src.mathapp.app import Calculator
 
-@pytest.fixture(scope="module")
-def calc():
+@pytest.fixture
+def calculator():
     return Calculator(2,4)
 
+@pytest.fixture(scope="module", params=[(2,3), (4,5)])
+def custom_calculator(request):
+    a,b = request.param
+    return Calculator(a, b)
 
-@pytest.fixture(scope="module")
-def custom_calc():
-    def _calc(a, b):
+@pytest.fixture
+def custom_param():
+    def _calculator(a, b):
         return Calculator(a, b)
 
-    return _calc
-
+    return _calculator
--- rootpy/tests/mathapp/test_math.py
+++ rootpy/tests/mathapp/test_math.py
@@ -1,19 +1,21 @@
 import pytest
 
-from src.mathapp.app import Calculator
+def test_add(calculator):
+    assert calculator.add() == 6
 
-calculator = Calculator(4, 2)
+def test_sub(calculator):
+    assert calculator.sub() == -2
 
-def test_add(calc):
-    assert calc.add() == 6
-
-
-def test_divide_by_zero(calc):
-    calc.b = 0
+def test_divide_by_zero(calculator):
+    calculator.b = 0
     with pytest.raises(ZeroDivisionError):
-        calc.div()
+        calculator.div()
 
-# advanced calculator testing with dynamic fixture
+def test_add_param(custom_calculator):
+    assert custom_calculator.add() == 5
 
-def test_add_advance(custom_calc):
-    assert custom_calc(10,20).add() == 30
+def test_add_custom_param(custom_param):
+    assert custom_param(10,20).add() == 30
+
+def test_sub_custom_param(custom_param):
+    assert custom_param(10,20).sub() == 30
