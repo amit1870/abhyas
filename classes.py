f4500bc962179ab63d1160a5717e8d8b191fcf76
amit1870 <amitxvf@gmail.com>
2024-03-04 10:45:28 +0000

    sitaram : property getter, setter and deleter

--- classes.py
+++ classes.py
@@ -14,14 +14,25 @@
 # what is an abstract class ?
 # abstract class is an interface class which should be implemented(all) by sub/child classes.
 
-# decorators with classes : @classmethod, @staticmethod, @abstractmethod
+
+# decorators with classes : @classmethod, @staticmethod, @abstractmethod, @property
+
+# @abstractmethod : to declare a method as abstract method
+
 # @classmethod : transform a method into a class method.
-# class method receives the class as an implicit first argument(cls).
-# C.f()/C().f() : if a class method is called for a derived class,
-# the derived class object is passed as the implied first argument.
+#              : class method receives the class as an implicit first argument(cls).
+#              : C.f()/C().f()
+#              : if a class method is called for a derived class,
+#              : the derived class object is passed as the implied first argument.
+
 # @staticmethod : transform a method into a static method.
-# a static method does not receive an implicit first argument.
-# can be called C.f()/C().f()
+#               : a static method does not receive an implicit first argument.
+#               : can be called C.f()/C().f()
+
+# @property     : used to create getter, setter, deleter, doc
+#               : property(fget, fset, fdel, doc)
+#               : using property decorator can avoid breaking client code
+
 
 
 from abc import ABC, abstractmethod
@@ -74,6 +85,9 @@ class Child(Parent):
     def set_profession(self, profession):
         self.profession = profession
 
+    def __eq__(self, child):
+        return self.father.name == child.father.name
+
     @staticmethod
     def self_learning():
         return ['Python', 'Pytest', 'Linux']
@@ -88,6 +102,63 @@ print(child.profession)
 child.set_profession('bramhan')
 print(child.profession)
 print(Child.self_learning())
+
+ram = Child('ram', father)
+laxman = Child('laxman' , father)
+
+print(ram == laxman)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __eq__(self, person):
+        return self.name == person.name and self.age == person.age
+
+ram = Person('ram', 18)
+laxman = Person('laxman', 18)
+ramanuj = Person('laxman', 18)
+print(ram == laxman)
+print(ramanuj == laxman)
+
+# @property : getter, setter, deleter
+
+class Ram:
+    def __init__(self, prefix, name):
+        self._name = f"{prefix}{name}"
+
+    @property   # getter
+    def name(self):
+        return self._name
+
+    @name.setter    # setter
+    def name(self, name):
+        words = name.split(":")
+        prefix, name = words[0], words[1]
+        if prefix not in ['sita', 'radha', 'janki', 'rukmani', 'laxmi']:
+            print(f"{prefix} not valid for setting ram")
+            return
+        self._name = f"{prefix}{name}"
+
+    @name.deleter   # deleter
+    def name(self):
+        self._name = 'sitaram'
+
+
+sitaram = Ram('sita','ram')
+print(sitaram.name)
+sitaram.name = "sita:ram"
+print(sitaram.name)
+sitaram.name = "radha:ram"
+print(sitaram.name)
+del sitaram.name
+print(sitaram.name)
+
+
+
+
+
 
 
 
