7720a64527711023c34c52c080e48ae3d5e1c036
amit1870 <amitxvf@gmail.com>
2024-03-05 05:33:07 +0000

    sitaram : print for request.config and pytestconfig

--- fastest/tests/pyapp/test_pyapp.py
+++ fastest/tests/pyapp/test_pyapp.py
@@ -3,11 +3,11 @@ import pytest
 def test_password_generator(get_strong_password, pytestconfig):
     # print(pytestconfig.option) # will include config added from cmd
     password = get_strong_password
-
     assert pytestconfig.password_pattern.match(password), "password does not match specs"
 
 @pytest.mark.cli
 def test_password_cli(request):
+    # print(request.config.option) # will include config added from cmd
     password = request.config.getoption("--password")
     assert request.config.password_pattern.match(password), "password does not match specs"
 
@@ -15,12 +15,10 @@ def test_password_cli(request):
 @pytest.mark.xfail(reason="simple password should not pass check")
 def test_password_simple(get_simple_password, pytestconfig):
     password = get_simple_password
-
     assert pytestconfig.password_pattern.match(password), "password does not match specs"
 
 @pytest.mark.xfail(reason="strong password should pass check")
 def test_password_strong(pytestconfig):
     password = '4BG%GSF98#$jdh'
-
     assert pytestconfig.password_pattern.match(password), "password does not match specs"
 
