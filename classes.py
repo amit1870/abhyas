7daa95a9679da3bed5e45eb03c784880de2ae307
amit1870 <amitxvf@gmail.com>
2024-03-05 12:06:59 +0000

    sitaram : implementation of class with inheritance

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 # what is OOPs concept ?
 # encapsulation : encapsulating data and functionality together.
 # inheritance   : inherting property and methods of parents
+#               : inheritance is an is-a relationship
+#               : apple is-a Fruit, car is-a vehicle, dog is-a animal
 # polymorphism  : same object acting differently with scenario change
 
 # what is class and object ?
@@ -14,6 +16,12 @@
 # what is an abstract class ?
 # abstract class is an interface class which should be implemented(all) by sub/child classes.
 
+# what is method overriding ?
+# the method in the subclass overrides the method in the superclass. concept called overriding.
+
+# what is method resolution order MRO in case of multiple inheritance ?
+# method resolution is done with left most class. if found a method there it will be returned.
+# otherwise it will follow order of left-to-right for MRO.
 
 # decorators with classes : @classmethod, @staticmethod, @abstractmethod, @property
 
@@ -37,11 +45,12 @@
 
 from abc import ABC, abstractmethod
 
-class Human(ABC):
-    def __init__(self, legs=2, hands=2, eyes=2):
+class SitaRam(ABC):
+    def __init__(self, legs=None, hands=None, eyes=None, gender=None):
         self.legs = legs
         self.hands = hands
         self.eyes = eyes
+        self.gender = gender
 
     @abstractmethod
     def is_blind(self):
@@ -52,113 +61,180 @@ class Human(ABC):
         pass
 
 
-
-class Student(Human):
-    def __init__(self, legs, hands, eyes):
-        super().__init__(legs, hands, eyes)
+class Manushya(SitaRam):
+    def __init__(self, legs, hands, eyes, gender='PLNG'):
+        super().__init__(legs, hands, eyes, gender)
 
     def is_blind(self):
-        return self.eyes < 2
+        return self.eyes == 0
 
     def is_disabled(self):
-        return self.hands < 2 or self.legs < 2
+        return self.legs < 2 or self.hands < 2
 
-    def eye_operation(self):
-        self.eyes = 2
+    def do_dharm(self):
+        print("do manushya dharm as in manu smrti")
 
+class SatRupa(SitaRam):
+    def __init__(self, legs, hands, eyes, gender='SLNG'):
+        super().__init__(legs, hands, eyes, gender)
 
+    def is_blind(self):
+        return self.eyes == 0
 
-class Parent:
-    def __init__(self, name, profession):
+    def is_disabled(self):
+        return self.legs < 2 or self.hands < 2
+
+    def do_dharm(self):
+        print("do manushya ki seva with dharm")
+
+class Braman(Manushya, SatRupa):
+    def __init__(self, name, legs, hands, eyes, gender):
+        super().__init__(legs, hands, eyes, gender)
         self.name = name
-        self.profession = profession
 
-    def get_profession(self):
-        return self.profession
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do sandhya vandan and guide people')
 
-class Child(Parent):
-    def __init__(self, name, father):
+        else:
+            print('do pati ki seva')
+
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
+
+    def generate_name(self):
+        return f"braman {self.name}"
+
+
+class Chatriya(Manushya, SatRupa):
+    def __init__(self, name, legs, hands, eyes, gender):
+        super().__init__(legs, hands, eyes, gender)
         self.name = name
-        self.father = father
-        self.profession = father.profession
 
-    def set_profession(self, profession):
-        self.profession = profession
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do rajdharm and protect braman')
 
-    def __eq__(self, child):
-        return self.father.name == child.father.name
+        else:
+            print('do pati ki seva')
 
-    @staticmethod
-    def self_learning():
-        return ['Python', 'Pytest', 'Linux']
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
 
-father = Parent('raja dasaratha', 'kshatriya')
-child = Child('sitaram', father)
-print(child)
-print(child.name)
-print(child.father.name)
-print(child.father.profession)
-print(child.profession)
-child.set_profession('bramhan')
-print(child.profession)
-print(Child.self_learning())
+    def generate_name(self):
+        return f"chatriya {self.name}"
 
-ram = Child('ram', father)
-laxman = Child('laxman' , father)
 
-print(ram == laxman)
-
-class Person:
-    def __init__(self, name, age):
+class Vaishya(Manushya, SatRupa):
+    def __init__(self, name, legs, hands, eyes, gender):
+        super().__init__(legs, hands, eyes, gender)
         self.name = name
-        self.age = age
 
-    def __eq__(self, person):
-        return self.name == person.name and self.age == person.age
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do vaishya dharm and help raj dharm')
 
-ram = Person('ram', 18)
-laxman = Person('laxman', 18)
-ramanuj = Person('laxman', 18)
-print(ram == laxman)
-print(ramanuj == laxman)
+        else:
+            print('do pati ki seva')
 
-# @property : getter, setter, deleter
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
 
-class Ram:
-    def __init__(self, prefix, name):
-        self._name = f"{prefix}{name}"
-
-    @property   # getter
-    def name(self):
-        return self._name
-
-    @name.setter    # setter
-    def name(self, name):
-        words = name.split(":")
-        prefix, name = words[0], words[1]
-        if prefix not in ['sita', 'radha', 'janki', 'rukmani', 'laxmi']:
-            print(f"{prefix} not valid for setting ram")
-            return
-        self._name = f"{prefix}{name}"
-
-    @name.deleter   # deleter
-    def name(self):
-        self._name = 'sitaram'
+    def generate_name(self):
+        return f"vaishya {self.name}"
 
 
-sitaram = Ram('sita','ram')
-print(sitaram.name)
-sitaram.name = "sita:ram"
-print(sitaram.name)
-sitaram.name = "radha:ram"
-print(sitaram.name)
-del sitaram.name
-print(sitaram.name)
+class Sudra(Manushya, SatRupa):
+    def __init__(self, name, legs, hands, eyes, gender):
+        super().__init__(legs, hands, eyes, gender)
+        self.name = name
+
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do sudra dharm and help vaishya dharm')
+        else:
+            print('do pati ki seva')
+
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
+
+    def generate_name(self):
+        return f"sudra {self.name}"
 
 
+class Kashyap(Braman):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+class Vashitha(Braman):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+class Raguvans(Chatriya):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+class Maurya(Chatriya):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+class Kurmi(Vaishya):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do kheti with bailo ki dekhbhal ke sath and help vaishya samaj')
+        else:
+            print('do pati ki seva')
+
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
+
+class Kushwaha(Vaishya):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+    def _perform_dharm(self, gender):
+        if gender == 'PLNG':
+            print('do beej with balo ki dekhbhal ke sath and help vaishya samaj')
+        else:
+            print('do pati ki seva')
+
+    def do_dharm(self):
+        self._perform_dharm(self.gender)
+
+class Charmkar(Sudra):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
+
+class Vanvasi(Sudra):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
 
 
+class VarnSankar(Kushwaha,Kurmi):
+    def __init__(self, name, legs, eyes, hands, gender):
+        super().__init__(name, legs, eyes, hands, gender)
 
+ramanuj = Kashyap('ramanuj',2,2,2,'PLNG')
+ramanand = Vashitha('ramanand',2,2,2,'PLNG')
+ram = Raguvans('ram',2,2,2,'PLNG')
+chandragupta = Maurya('chandragupta',2,2,2,'PLNG')
+amit = Kurmi('amit',2,2,2,'PLNG')
+nishad = Vanvasi('nishad',2,2,2,'PLNG')
+rahul = Kushwaha('rahul',2,2,2,'PLNG')
+sankar = VarnSankar('sankar',2,2,2,'PLNG')
 
+print(ramanuj)
+ramanuj.do_dharm()
 
+print(amit)
+amit.do_dharm()
+
+print(rahul)
+rahul.do_dharm()
+
+print(sankar)
+sankar.do_dharm()
 
