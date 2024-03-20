b0e62849ec69c39dda44f90a88b0367514bea564
amit1870 <amitxvf@gmail.com>
2024-03-20 05:37:44 +0000

    sitaram : added algolevel

--- algolevel.py
+++ algolevel.py
@@ -15,3 +15,17 @@ array = [4, 7, 8, 1, 7, 8, 9]
 targetsum = 23
 t = choose_triplet(array, targetsum)
 # print(t)
+
+def search_pair(array):
+    paired = []
+    for i, vi in enumerate(array):
+        for j, vj in enumerate(array):
+            if vi + vj in array:
+                paired.append(vi + vj)
+
+    return paired
+
+
+array = ['radha', 'krishna', 'radhakrishna', 'anuj', 'ram' ,'ramanuj', 'bharat', 'bharatanuj']
+t = search_pair(array)
+print(t)
