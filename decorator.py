ffcf7f6012f6c042dfc15c32b33c3f432e8f6768
amitxvf <amitxvf@gmail.com>
2024-03-29 04:52:13 +0000

    sitaram : decorator refreshed with sitaram

--- decorator.py
+++ decorator.py
@@ -8,87 +8,75 @@
 # function  : function are first class objects and objects can assigned/returned.
 
 # decorator without arguments
-def add_sitaram(func):
-    def append_sitaram():
-        sitarams = []
-        names = func()
-        for name in names:
-            sitarams.append(f"jay sitaram {name}")
 
-        return sitarams
-
-    return append_sitaram
-
-@add_sitaram
-def get_names():
-    return ['amit', 'ajay', 'avinash', 'rahul', 'vivek', 'bharat', 'aman', 'bacchi', 'narayan']
-
-
-sitaram_added_names = get_names()
-print(sitaram_added_names)
-
-def get_names():
-    return ['amit', 'ajay', 'avinash', 'rahul', 'vivek', 'bharat', 'aman', 'bacchi', 'narayan']
-
-
-add_sitaram_to_names = add_sitaram(get_names)
-sitaram_added_names = add_sitaram_to_names()
-print(sitaram_added_names)
-
-# decorator with arguments
-
-def add_radha(func):
-    def wrapper(names):
-        radha_names = []
-        names = func(names)
-        for name in names:
-            radha_names.append(f"radha {name}")
-
-        return radha_names
+def jay_sitaram(func):
+    def wrapper():
+        return [f"say sitaram {name}" for name in func()]
 
     return wrapper
 
-@add_radha
-def get_names(initial):
-    if initial.lower() == 'a':
-        return ['amit', 'anil', 'avadha', 'arun', 'ankur']
-    elif initial.lower() == 'b':
-        return ['bharat', 'bhuwan', 'bhumi', 'bhuvnesh','bansi']
-    else:
-        return ['sitaram', 'radhakrishna', 'hariom', 'narayan','ramanuj']
-
-radha_added_names = get_names('c')
-print(radha_added_names)
+@jay_sitaram
+def get_names():
+    return ['sachin', 'sunil', 'rajesh', 'brijesh', 'durgesh', 'amit', 'rajkumar']
 
 
-def get_name_by_inital(initial):
-    if initial.lower() == 'a':
-        return ['amit', 'anil', 'avadha', 'arun', 'ankur']
-    elif initial.lower() == 'b':
-        return ['bharat', 'bhuwan', 'bhumi', 'bhuvnesh','bansi']
-    else:
-        return ['sitaram', 'radhakrishna', 'hariom', 'narayan','ramanuj']
+names = get_names()
+print(names)
 
-radha_added_name = add_radha(get_name_by_inital)
-radha_added_names = radha_added_name('a')
-print(radha_added_names)
-radha_added_names = radha_added_name('b')
-print(radha_added_names)
-radha_added_names = radha_added_name('c')
-print(radha_added_names)
+# decorator with arguments
+
+def read_radha(func):
+    def wrapper(varn):
+        if varn == 'BMH':
+            return [f"{rishi} leads bharat" for rishi in func(varn)]
+
+        elif varn == 'KSH':
+            return [f"{ksh} protect bharat" for ksh in func(varn)]
+
+        elif varn == 'VSH':
+            return [f"{vsh} prosper bharat" for vsh in func(varn)]
+
+        return [f"{sdh} does sitaram" for sdh in func(varn)]
 
 
-def generate_number(a, b, n):
+    return wrapper
+
+@read_radha
+def do_dharm(varn):
+    if varn == 'BMH':
+        return ['guruvar', 'maharishi', 'vashithaji', 'vishwamitramuni', 'jabali', 'kashyap']
+
+    elif varn == 'KSH':
+        return ['sitaram', 'radhakrishna', 'lakshman', 'bharat', 'satrudhan']
+
+    elif varn == 'VSH':
+        return ['madhav', 'chaitnya', 'mahaprabhu', 'vallbha']
+
+    return ['amit', 'sachin', 'sunil', 'rajesh', 'rajkumar', 'brijesh']
+
+
+bmh = do_dharm('BMH')
+ksh = do_dharm('KSH')
+vsh = do_dharm('VSH')
+sdh = do_dharm('SDH')
+
+print(bmh)
+print(ksh)
+print(vsh)
+print(sdh)
+
+# decorator without @syntax
+
+def generate_number(a, b, count):
     import random
-    if b - a <= 0:
+    if b - a < count:
         return [a, b]
     else:
-        return random.sample(range(a,b+1), n)
+        return random.sample(range(a,b+1), count)
 
 def sum_number_list(func):
-    def wrapper(a, b, n):
-        numbers = func(a, b, n)
-        print(numbers)
+    def wrapper(a, b, count):
+        numbers = func(a, b, count)
         return sum(numbers)
 
     return wrapper
@@ -97,47 +85,6 @@ sum_number = sum_number_list(generate_number)
 total = sum_number(10, 20, 5)
 print(total)
 
-sum_number = sum_number_list(generate_number)
-total = sum_number(10, 20, 6)
-print(total)
-
-
-def x(func):
-    def wrapper(x):
-        return 4 * func(x)
-
-    return wrapper
-
-@x
-def func1(y):
-    return y
-
-print(func1(1)) #print 4
-print(func1(2)) #print 8
-print(func1(3)) #print 12
-
-def g(a):
-    return a - 10
-
-def d(f):
-    def w(x):
-        z = f(x)
-        return g(z)
-
-    return w
-
-def f(a):
-    return a
-
-a = 10
-df = d(f)
-dfa = df(a)
-print(dfa)
-
-b = 20
-df = d(f)
-dfb = df(b)
-print(dfb)
 
 # decroator with variable arguments
 
@@ -156,7 +103,7 @@ def sum_numbers(func):
     return wrapper
 
 
-squared = square_number(10,20, 30)
+squared = square_number(10,20,30)
 print(squared)
 
 sum_squared = sum_numbers(square_number)
@@ -166,22 +113,3 @@ print(summed)
 sum_squared = sum_numbers(square_number)
 summed = sum_squared(10, 20, 30)
 print(summed)
-
-def x(func_org):
-    def func(y):
-        print("go")
-        return 4 * y
-
-    return func
-
-
-
-
-def func1(y):
-    return y
-
-print(func1(1)) # 4
-
-ss = x(func1)
-print(ss(1))
-print(ss(2))
